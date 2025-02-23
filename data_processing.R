28718ced065d9a6399c22dcd74b4f05a447cfb2f
ErlonBoechat <ErlonBoechat@users.noreply.github.com>
2025-02-22 22:50:24 -0300

    13

--- data_processing.R
+++ data_processing.R
@@ -19,6 +19,13 @@ DataProcessor <- R6::R6Class("DataProcessor",
                                  raw_lines <- read_lines(file_path)
                                  raw_lines <- trimws(raw_lines)  # Remove espaços extras
                                  
+                                 for (line in raw_lines) {
+                                   
+                                   if (grepl("^[0-9]{2}:[0-9]{2} [0-9]{2}/[0-9]{2}/[0-9]{4}$", line)) {
+                                    current_datetime <- line 
+                                   }
+                                 }
+                                
                                  # Remover caracteres invisíveis antes do @
                                  profile_raw_lines <- gsub("^[^@]*", "", raw_lines)
                                  
@@ -31,16 +38,18 @@ DataProcessor <- R6::R6Class("DataProcessor",
                                  
                                  if (num_profiles == 0) {
                                    stop("Erro: Nenhum perfil identificado no arquivo. Verifique o formato.")
-                                  }
+                                 }
                                  
                                  print(paste("Perfis identificados:", num_profiles))
+                                 
+                                 current_datetime <- data.frame(Date.Time = rep(current_datetime, num_profiles))
                                  
                                  # Lista das métricas esperadas
                                  metric_names <- c("Media Uploads", "Followers", "Following", "Engagement Rate", 
                                                    "AVG Likes", "AVG Comments")
                                  
                                  profile_data <- list() # Criar estrutura para armazenar dados do perfil
-                                 profiles_data <- list()# Criar estrutura para armazenar os dados processados
+                                 profiles_data <- list() # Criar estrutura para armazenar os dados processados
 
                                  first_metric_position <- 5  # Linha onde começam as métricas do primeiro perfil
                                  r <- 47  # Razão fixa de espaçamento entre perfis
@@ -55,19 +64,17 @@ DataProcessor <- R6::R6Class("DataProcessor",
                                          value <- gsub("[^0-9.-]", "", raw_lines[metric_index])  # Extrair números
                                          profile_data[[metric_names[j]]] <- as.numeric(value)  # Converter para número
                                       } 
-                                      
                                       else {
-                                      
                                          profile_data[[metric_names[j]]] <- NA  # Evitar erro caso a linha não exista
                                       }
                                    }
-                                   
                                      profiles_data[[Perfil = profile_names[i]]] <- profile_data # Armazena os dados do perfil na lista final
                                  }
                                  
                                  # Converter a lista para um dataframe final
                                  final_data <- do.call(rbind, lapply(profiles_data, as.data.frame))
-                                 
+                                 final_data <- cbind(Date.Time = current_datetime, final_data)
+
                                  if (is.null(final_data) || nrow(final_data) == 0) {
                                    stop("Erro: Nenhum dado foi extraído do arquivo ", file_path)
                                  }
